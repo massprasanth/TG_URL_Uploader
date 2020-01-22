ac84328a9a4dc3c1a2bd081ed61c8c9857b410ec
aftabkh505 <38653077+aftabkh505@users.noreply.github.com>
2020-01-23 01:02:57 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -27,7 +27,7 @@ class Config(object):
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("http://117.240.238.230/", "")
     # https://t.me/hevcbay/951
-    OUO_IO_API_KEY = ""
+    OUO_IO_API_KEY = "4ed43d52342463ad74846b57a28bdc4e"
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
